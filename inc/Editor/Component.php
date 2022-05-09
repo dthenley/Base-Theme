fec3ee3552af5e76274ad739dcc7b4a18aa1f86d
Donte Henley <3503886+dthenley@users.noreply.github.com>
2022-05-09 14:07:28 -0500

    Add Option  to remove block css

--- inc/Editor/Component.php
+++ inc/Editor/Component.php
@@ -32,6 +32,7 @@ class Component implements Component_Interface {
 	 */
 	public function initialize() {
 		add_action( 'after_setup_theme', array( $this, 'action_add_editor_support' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'dequeue_block_styles' ) );
 	}
 
 	/**
@@ -58,66 +59,67 @@ class Component implements Component_Interface {
 		 * Add the line below to disable the custom color picker in the editor.
 		 * add_theme_support( 'disable-custom-colors' );
 		 */
-		add_theme_support(
-			'editor-color-palette',
-			array(
-				array(
-					'name'  => __( 'Primary', 'wp-rig' ),
-					'slug'  => 'theme-primary',
-					'color' => '#e36d60',
-				),
-				array(
-					'name'  => __( 'Secondary', 'wp-rig' ),
-					'slug'  => 'theme-secondary',
-					'color' => '#41848f',
-				),
-				array(
-					'name'  => __( 'Red', 'wp-rig' ),
-					'slug'  => 'theme-red',
-					'color' => '#C0392B',
-				),
-				array(
-					'name'  => __( 'Green', 'wp-rig' ),
-					'slug'  => 'theme-green',
-					'color' => '#27AE60',
-				),
-				array(
-					'name'  => __( 'Blue', 'wp-rig' ),
-					'slug'  => 'theme-blue',
-					'color' => '#2980B9',
-				),
-				array(
-					'name'  => __( 'Yellow', 'wp-rig' ),
-					'slug'  => 'theme-yellow',
-					'color' => '#F1C40F',
-				),
-				array(
-					'name'  => __( 'Black', 'wp-rig' ),
-					'slug'  => 'theme-black',
-					'color' => '#1C2833',
-				),
-				array(
-					'name'  => __( 'Grey', 'wp-rig' ),
-					'slug'  => 'theme-grey',
-					'color' => '#95A5A6',
-				),
-				array(
-					'name'  => __( 'White', 'wp-rig' ),
-					'slug'  => 'theme-white',
-					'color' => '#ECF0F1',
-				),
-				array(
-					'name'  => __( 'Dusty daylight', 'wp-rig' ),
-					'slug'  => 'custom-daylight',
-					'color' => '#97c0b7',
-				),
-				array(
-					'name'  => __( 'Dusty sun', 'wp-rig' ),
-					'slug'  => 'custom-sun',
-					'color' => '#eee9d1',
-				),
-			)
-		);
+
+		// add_theme_support(
+		// 	'editor-color-palette',
+		// 	array(
+		// 		array(
+		// 			'name'  => __( 'Primary', 'wp-rig' ),
+		// 			'slug'  => 'theme-primary',
+		// 			'color' => '#e36d60',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Secondary', 'wp-rig' ),
+		// 			'slug'  => 'theme-secondary',
+		// 			'color' => '#41848f',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Red', 'wp-rig' ),
+		// 			'slug'  => 'theme-red',
+		// 			'color' => '#C0392B',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Green', 'wp-rig' ),
+		// 			'slug'  => 'theme-green',
+		// 			'color' => '#27AE60',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Blue', 'wp-rig' ),
+		// 			'slug'  => 'theme-blue',
+		// 			'color' => '#2980B9',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Yellow', 'wp-rig' ),
+		// 			'slug'  => 'theme-yellow',
+		// 			'color' => '#F1C40F',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Black', 'wp-rig' ),
+		// 			'slug'  => 'theme-black',
+		// 			'color' => '#1C2833',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Grey', 'wp-rig' ),
+		// 			'slug'  => 'theme-grey',
+		// 			'color' => '#95A5A6',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'White', 'wp-rig' ),
+		// 			'slug'  => 'theme-white',
+		// 			'color' => '#ECF0F1',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Dusty daylight', 'wp-rig' ),
+		// 			'slug'  => 'custom-daylight',
+		// 			'color' => '#97c0b7',
+		// 		),
+		// 		array(
+		// 			'name'  => __( 'Dusty sun', 'wp-rig' ),
+		// 			'slug'  => 'custom-sun',
+		// 			'color' => '#eee9d1',
+		// 		),
+		// 	)
+		// );
 
 		/*
 		 * Add support custom font sizes.
@@ -125,34 +127,42 @@ class Component implements Component_Interface {
 		 * Add the line below to disable the custom color picker in the editor.
 		 * add_theme_support( 'disable-custom-font-sizes' );
 		 */
-		add_theme_support(
-			'editor-font-sizes',
-			array(
-				array(
-					'name'      => __( 'Small', 'wp-rig' ),
-					'shortName' => __( 'S', 'wp-rig' ),
-					'size'      => 16,
-					'slug'      => 'small',
-				),
-				array(
-					'name'      => __( 'Medium', 'wp-rig' ),
-					'shortName' => __( 'M', 'wp-rig' ),
-					'size'      => 25,
-					'slug'      => 'medium',
-				),
-				array(
-					'name'      => __( 'Large', 'wp-rig' ),
-					'shortName' => __( 'L', 'wp-rig' ),
-					'size'      => 31,
-					'slug'      => 'large',
-				),
-				array(
-					'name'      => __( 'Larger', 'wp-rig' ),
-					'shortName' => __( 'XL', 'wp-rig' ),
-					'size'      => 39,
-					'slug'      => 'larger',
-				),
-			)
-		);
+		// add_theme_support(
+		// 	'editor-font-sizes',
+		// 	array(
+		// 		array(
+		// 			'name'      => __( 'Small', 'wp-rig' ),
+		// 			'shortName' => __( 'S', 'wp-rig' ),
+		// 			'size'      => 16,
+		// 			'slug'      => 'small',
+		// 		),
+		// 		array(
+		// 			'name'      => __( 'Medium', 'wp-rig' ),
+		// 			'shortName' => __( 'M', 'wp-rig' ),
+		// 			'size'      => 25,
+		// 			'slug'      => 'medium',
+		// 		),
+		// 		array(
+		// 			'name'      => __( 'Large', 'wp-rig' ),
+		// 			'shortName' => __( 'L', 'wp-rig' ),
+		// 			'size'      => 31,
+		// 			'slug'      => 'large',
+		// 		),
+		// 		array(
+		// 			'name'      => __( 'Larger', 'wp-rig' ),
+		// 			'shortName' => __( 'XL', 'wp-rig' ),
+		// 			'size'      => 39,
+		// 			'slug'      => 'larger',
+		// 		),
+		// 	)
+		// );
+	}
+
+	/**
+	 * Removes Default Gutenberg Block Styles.
+	 */
+	public function dequeue_block_styles() {
+		// wp_dequeue_style( 'wp-block-library' );
+		// wp_dequeue_style( 'wp-block-library-theme' );
 	}
 }
