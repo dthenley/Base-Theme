de115d797a161355f117293655b9e9784659dbd4
Donte Henley <3503886+dthenley@users.noreply.github.com>
2022-05-12 11:03:33 -0500

    Adding in a 2nd version of footer

--- template-parts/footer/sidebar-2.php
+++ template-parts/footer/sidebar-2.php
@@ -14,13 +14,8 @@ if ( ! wp_rig()->is_footer_nav_menu_active() ) {
 <div class="main-footer">
 	<?php the_custom_logo(); ?>
 	<nav>
-		<?php
-		wp_rig()->display_footer_nav_menu(
-			array(
-				'menu_id' => 'primary-menu',
-				'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s<li class="social-media-main-nav">'.do_shortcode("[yoast-social-media]").'</li></ul>'
-			)
-		);
-		?>
+		<?php wp_rig()->display_footer_nav_menu( array( 'menu_id' => 'footer-menu' ) ); ?>
 	</nav>
+	<?php echo do_shortcode('[yoast-social-media]'); ?>
+
 </div>
