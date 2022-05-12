b6b1f7764703f1c4c024c22591bc3d03e98c8514
Donte Henley <3503886+dthenley@users.noreply.github.com>
2022-05-12 10:22:54 -0500

    Modify copyright section

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -10,19 +10,13 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ); ?>">
-		<?php
-		/* translators: %s: CMS name, i.e. WordPress. */
-		printf( esc_html__( 'Proudly powered by %s', 'wp-rig' ), 'WordPress' );
-		?>
-	</a>
-	<span class="sep"> | </span>
+	&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 	<?php
-	/* translators: Theme name. */
-	printf( esc_html__( 'Theme: %s by the contributors.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>' );
-
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '<span class="sep"> | </span>' );
+		the_privacy_policy_link( '', '<span class="sep"> | </span>' );
 	}
 	?>
+	<span class="sep"> | </span>
+
+	<?php printf( esc_html__( 'Site by %s ', 'wp-rig' ), '<a href="' . esc_url( 'Site by Hexcode Designs, LLC' ) . '">Hexcode Designs, LLC</a>' ); ?>
 </div><!-- .site-info -->
