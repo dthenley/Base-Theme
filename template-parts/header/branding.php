28e7c29efd13430ebef8692a60c4b06d48db64bc
Donte Henley <3503886+dthenley@users.noreply.github.com>
2022-06-22 16:25:51 -0500

    update logo

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -10,5 +10,16 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-branding">
-	<?php the_custom_logo(); ?>
+	<a href="/" class="custom-logo-link" rel="home" aria-current="<?php echo get_post_type( get_the_id() ); ?>">
+		<?php
+		if ( has_custom_logo() ) {
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+			echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '" width="200" height="58">';
+		} else {
+			echo '<p class="text-logo">' . get_bloginfo('name') . '</p>';
+
+		}
+		?>
+	</a>
 </div><!-- .site-branding -->
