d6b2aaedb35bd3424ed358fbcb342b6b6826c1ed
Donte <3503886+dthenley@users.noreply.github.com>
2022-11-16 11:25:47 -0600

    Update info.php

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -11,12 +11,13 @@ namespace WP_Rig\WP_Rig;
 
 <div class="site-info">
 	&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+
+	<span class="sep"> | </span>
+
 	<?php
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
 		the_privacy_policy_link( '', '<span class="sep"> | </span>' );
 	}
 	?>
-	<span class="sep"> | </span>
-
 	<?php printf( esc_html__( 'Site by %s ', 'wp-rig' ), '<a href="' . esc_url( 'https://hexcodedesigns.com/' ) . '" target="_blank">Hexcode Designs, LLC</a>' ); ?>
 </div><!-- .site-info -->
