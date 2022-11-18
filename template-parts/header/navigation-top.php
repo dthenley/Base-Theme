2a3356ad60bce52aa66e00df87cf480eadff62da
Donte Henley <3503886+dthenley@users.noreply.github.com>
2022-11-18 14:03:13 -0600

    Navigation changes

--- template-parts/header/navigation-top.php
+++ template-parts/header/navigation-top.php
@@ -15,10 +15,12 @@ if ( ! wp_rig()->is_top_left_nav_menu_active() || ! wp_rig()->is_top_right_nav_m
 
 <nav id="top-navigation" class="top-navigation" aria-label="<?php esc_attr_e( 'Top Navigation', 'wp-rig' ); ?>">
 
-
+	<div class="top-left-nav-menu">
 		<?php wp_rig()->display_top_left_nav_menu(); ?>
+	</div>
 
+	<div class="top-right-nav-menu">
 		<?php wp_rig()->display_top_right_nav_menu(); ?>
-
+	</div>
 
 </nav><!-- #top-navigation -->
