efdbc398682a5a265c0a4eaa94cb655935d7ca05
Donte Henley <3503886+dthenley@users.noreply.github.com>
2022-12-02 16:29:01 -0600

    Add Patterns

---
+++ patterns/cta.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Call To Action
+ * Slug: wp-rig/call-to-action
+ * Categories: layout
+ * Description: Call to action
+ * Block Types: core/group
+ * Keywords: cta, call-to-action
+ *
+ * @package wp_rig
+ */
+
+?>
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"40rem"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center">We're never too busy to connect with you</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained","wideSize":"35rem"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Have a question? Have a request? Need a solution? <br>You can contact us at any time.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" href="/contact/">Contact now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
---
+++ patterns/page-header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Page Header
+ * Slug: wp-rig/page-header
+ * Categories: layout
+ * Description: Page Header
+ * Block Types: core/cover
+ * Keywords: header
+ *
+ * @package wp_rig
+ */
+
+?>
+<!-- wp:cover {"url":"/wp-content/uploads/2022/11/Homepage-Header.jpg","id":30,"dimRatio":70,"overlayColor":"primary","minHeight":40,"minHeightUnit":"rem","align":"full","className":"is-style-styled-cover"} -->
+<div class="wp-block-cover alignfull is-style-styled-cover" style="min-height:40rem"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-30" alt="Homepage Header" src="/wp-content/uploads/2022/11/Homepage-Header.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","wideSize":"40rem"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="has-text-align-center">Headline</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center">Sub Headline</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Tagline</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Button</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
